207274c97d61eab5a0b9225973ec4e9a80cea344
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2023-02-09 02:30:55 +0200

    Extend AspNetUser Table

---
+++ TechCompany/TechCompany/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechCompany.Data
+{
+
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(250)]
+        public string Address1 { get; set; }
+
+        [StringLength(250)]
+        public string Address2 { get; set; }
+
+        [StringLength(50)]
+        public string PostCode { get; set; }
+
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ TechCompany/TechCompany/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
